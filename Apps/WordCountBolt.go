f1a0f4b3df00c2bbfe7ca74875475869d616aa89
Yuan <hyuan13@fa18-cs425-g69-01.cs.illinois.edu>
2018-11-20 16:55:48 -0600

    implement WordCountBolt

--- Apps/WordCountBolt.go
+++ Apps/WordCountBolt.go
@@ -1,5 +1,10 @@
 package Apps
 
+import (
+	"strings"
+	"strconv"
+)
+
 type WordCountBolt struct {
 	
 }
@@ -9,9 +14,24 @@ type WordCountBolt struct {
  * @param [line number] : [line content]
  * @return [word] : [count] 
  */
-func (self *WordCountBolt) Execute(map[string]string) map[string]string {
-	
+func (self *WordCountBolt) Execute(in map[string]string) map[string]string {
+	linenumber := in["linenumber"]
+	sentence := in["line"]
+	words := strings.Split(sentence, " ")
+	m := make(map[string]int)
+	for _, word := range words {
+		if _, ok := m[word]; ok {
+			m[word] += 1
+		} else {
+			m[word] = 1
+		}
+	}
+	out := make(map[string]string)
+	out["linenumber"] = linenumber
+	ret := ""
+	for word, count := range m {
+		ret += word + ":" + strconv.Itoa(count) + " "
+	}
+	out["lcounts"] = ret
+	return out
 }
-
-
-
