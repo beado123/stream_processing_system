edfea01917334d6f034ea39766cffc8345018b18
Yuan <hyuan13@fa18-cs425-g69-01.cs.illinois.edu>
2018-11-22 14:06:24 -0600

    add receive nimbus message

--- client.go
+++ client.go
@@ -6,9 +6,11 @@ import (
 	"bufio"
 	"fmt"
 	"strings"
+	"net"
+	"io/ioutil"
 )
 
-//TCP: 5678, UDP:3456
+//TCP: 6666, UDP:3333
 func main() {
 	if len(os.Args) < 2 {
 		fmt.Println("Please type in master id!")
@@ -38,6 +40,7 @@ func main() {
         			go d.TimeOutCheck()
         			go d.DaemonListenUDP()
 				go d.DaemonListenTCP()
+				go ListenFromNimbus()
 
 			} else if strings.Contains(cmd, "LIST") {
                                 d.PrintMembershipList()
@@ -55,6 +58,13 @@ func main() {
 				d.SendLsRequest(cmd)
 			} else if strings.Contains(cmd, "store") {
 				d.StoreRequest()
+			} else if strings.Contains(cmd, "wordcount") {
+				conn, err := net.Dial("tcp", "fa18-cs425-g69-" + master_id + ".cs.illinois.edu:8000")
+	        		if err != nil {
+        		        	fmt.Println(err)
+        			}
+				conn.Write([]byte(cmd))
+				conn.Close()
 			} else {
 				fmt.Println("Input does not match any commads!")	
 			}
@@ -62,3 +72,90 @@ func main() {
 	}
 }
 
+//port number 8000
+func ListenFromNimbus() {
+	ip_address := getIPAddrAndLogfile()
+	l, err := net.Listen("tcp", ip_address + ":8000")
+	if err != nil {
+		fmt.Println(err)
+                return
+	}
+
+	for true {
+		conn, err := l.Accept()
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		go ParseRequest(conn)
+	}
+}
+
+func ParseRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	reqLen, _ := conn.Read(buf)
+	reqArr := strings.Split(string(buf[:reqLen]), " ")
+	if reqArr[0] == "boltc" {
+		t := reqArr[0]
+		app := reqArr[1]
+		var children []string
+		for i, child := range reqArr {
+			if i == 0 || i == 1 {
+				continue
+			}
+			children = append(children, child)
+		}
+		fmt.Println(t)
+		fmt.Println(app)
+		for _, curr := range children {
+			fmt.Println(curr)
+		}
+		//bolt := bolt.NewBolt(t, app, children)
+		//go BoltListen()
+		
+	} else if reqArr[0] == "boltl" {
+		t := reqArr[0]
+		app := reqArr[1]
+                var children []string
+		fmt.Println(t)
+                fmt.Println(app)
+		for _, curr := range children {
+                        fmt.Println(curr)
+                }
+		//bolt := bolt.NewBolt(t, app, children)
+		//go BoltListen()
+	} else if reqArr[0] == "spout" {
+		t := reqArr[0]
+                app := reqArr[1]
+                var children []string
+                for i, child := range reqArr {
+                        if i == 0 || i == 1 {
+                                continue
+                        }
+                        children = append(children, child)
+                }
+		fmt.Println(t)
+                fmt.Println(app)
+                for _, curr := range children {
+                        fmt.Println(curr)
+                }
+		//spout := spout.NewSpout(t, app, children)
+		//go SpoutStarting()
+	}
+}
+
+func getIPAddrAndLogfile() string{
+	data, err := ioutil.ReadFile("ip_address")
+	if err != nil {
+		panic(err)
+	}
+
+	ip := string(data[:len(data)])
+	
+	//remove \n from end of line
+	if strings.HasSuffix(ip, "\n") {
+		ip = ip[:(len(ip) - 1)]
+	}
+	fmt.Println("ip address of current VM:\n", ip)
+	return ip
+}
