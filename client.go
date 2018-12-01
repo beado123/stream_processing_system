f68487fcb67950335cf78dc0b1ba57ed0e80da40
Zhang <yzhng249@fa18-cs425-g69-07.cs.illinois.edu>
2018-11-30 22:01:18 -0600

    client

--- client.go
+++ client.go
@@ -19,7 +19,7 @@ func main() {
 
 	appMap = make(map[string]string)
 	appMap["wordcount"] = "./wordcount_dataset"
-	appMap["reddit"] = "./reddit_dataset"
+	appMap["reddit"] = "./reddit_dataset.csv"
 
 	if len(os.Args) < 2 {
 		fmt.Println("Please type in master id!")
